61b65dddc83a084d040f6ba9f78d959c57dd96ea
Saeed Rs <sa33drs@gmail.com>
2021-01-24 16:32:22 +0330

    add frontend , backend and option resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\OptionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.frontend');
 });
+
+Route::get('/admin',function (){
+   return view('layouts.backend');
+});
+
+Route::resource('options', 'Admin\OptionController');
